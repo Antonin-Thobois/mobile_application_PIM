c8cefd6c575b0b301ce4dcf9950fa5ffc1c34373
vahe <anthony.odin8@gmail.com>
2022-07-04 16:48:15 +0200

    Mise en place du menu

--- App.tsx
+++ App.tsx
@@ -1,13 +1,21 @@
 import { NavigationContainer } from '@react-navigation/native';
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet } from 'react-native';
 import { RootNavigator } from './src/navigation/RootNavigator';
+import {Authentification, Home} from "./src/pages";
+import * as React from "react";
+import {createDrawerNavigator} from "@react-navigation/drawer";
+
+const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <RootNavigator />
-      <StatusBar style="auto" />
+        <Drawer.Navigator initialRouteName="Home">
+            <Drawer.Screen name="Home" component={Home}/>
+            <Drawer.Screen name="Authentification" component={Authentification}/>
+        </Drawer.Navigator>
+      {/*<RootNavigator />*/}
+      {/*<StatusBar style="auto" />*/}
     </NavigationContainer>
   );
 }
